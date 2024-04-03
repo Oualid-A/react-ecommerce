fe1a1258cb08c729a068278a6deb419f8cb7cace
Oualid-A <oualid.achrif@ump.ac.ma>
2024-04-03 14:27:42 +0200

    feat(Search): Add search input functionality to search for products

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import IconButton from "@mui/material/IconButton";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import InputAdornment from "@mui/material/InputAdornment";
+import FormControl from "@mui/material/FormControl";
+import SearchIcon from "@mui/icons-material/Search";
 
-const Search = () => {
+const Search = ({ onSearchChanged }) => {
   return (
-    <div>
-      search
-    </div>
-  )
-}
+    <FormControl
+      sx={{ mt: 2 }}
+      variant="outlined"
+      size="small"
+      className="w-full"
+    >
+      <InputLabel htmlFor="outlined-adornment-search">Search</InputLabel>
+      <OutlinedInput
+        onChange={(e) => onSearchChanged(e.target.value)}
+        id="outlined-adornment-search"
+        type="text"
+        endAdornment={
+          <InputAdornment position="end">
+            <IconButton aria-label="toggle search visibility" edge="end">
+              <SearchIcon />
+            </IconButton>
+          </InputAdornment>
+        }
+        label="Search"
+      />
+    </FormControl>
+  );
+};
 
-export default Search
+export default Search;
